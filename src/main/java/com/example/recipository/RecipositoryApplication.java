f1719595c0da5c06e811e6655bbbdee635efccad
Gyuhwan-Kim <keypoints94@gmail.com>
2022-09-13 22:15:29 +0900

    Start Project

---
+++ src/main/java/com/example/recipository/RecipositoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/recipository/RecipositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
